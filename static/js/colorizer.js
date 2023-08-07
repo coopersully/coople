92f9310572e6933da414ebe8835d34166275da58
Cooper Sullivan <thecoopersully@gmail.com>
2023-08-06 21:47:25 -0500

    Add shadow to homepage

--- static/js/colorizer.js
+++ static/js/colorizer.js
@@ -16,3 +16,13 @@ function darkenColor(color, percent) {
         B = (num & 0x0000FF) - amt;
     return "#" + (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 + (G < 255 ? G < 1 ? 0 : G : 255) * 0x100 + (B < 255 ? B < 1 ? 0 : B : 255)).toString(16).slice(1);
 }
+
+function colorizeRandomly(text) {
+    let colorizedText = '';
+    for (let i = 0; i < text.length; i++) {
+        let color = badgeColors[Math.floor(Math.random() * badgeColors.length)];
+        let darkColor = darkenColor(color, 20); // Darken by 20%
+        colorizedText += '<span style="color: ' + color + '; text-shadow: 0 1px ' + darkColor + ', 0 2px ' + darkColor + ', 0 3px ' + darkColor + ', 0 4px ' + darkColor + ', 0 5px ' + darkColor + ';">' + text[i] + '</span>';
+    }
+    return colorizedText;
+}
